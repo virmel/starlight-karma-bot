865e7933199d2044304ed46fe8b541ff7f5f4af2
virmel <virmelg@live.com>
2022-10-23 20:36:13 -0700

    all intents

--- starlight.py
+++ starlight.py
@@ -14,7 +14,7 @@ STARLIGHT_ID = 716834761418735638
 # rename variables
 # Refactor code - the section where karma is modified.
 
-client = discord.Client(intents=discord.Intents.default())
+client = discord.Client(intents=discord.Intents.all())
 
 
 async def handle(request):
